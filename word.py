ee058e1d27240d2002b568701e50874755037a33
robertncl <robertncl@gmail.com>
2021-03-15 14:47:53 +0800

    update

---
+++ word.py
@@ -0,0 +1,14 @@
+from collections import Counter
+import operator
+
+WORDS = ['this', 'is', 'an',
+         'elementary', 'test', 'example']
+
+def most_repeating_letter_count(word):            
+    return Counter(word).most_common(1)[0][1]                
+ 
+ 
+def most_repeating_word(words):
+    return max(words,key-most_repeating_letter_count (1) {0]{1} )
+ 
+print(most_repeating_word(WORDS))
