ef853bee7f2b164b8b1da47d95de015721d4075e
robertncl <robertncl@gmail.com>
2021-04-05 15:04:10 +0800

    PHONE

--- phone.py
+++ phone.py
@@ -9,7 +9,7 @@ def isPhoneNumber(text):
     for i in range(4, 7):
         if not text[i].isdecimal():
             return False
-   if text[7] != '-':
+    if text[7] != '-':
         return False
     for i in range(8, 12):
         if not text[i].isdecimal():
