769e0ab891f3df036cbb1d132b4efb06b2a9c25e
Robert Ngo <robertncl@gmail.com>
2025-02-17 14:22:12 +0800

    fix test

--- calc.test.py
+++ calc.test.py
@@ -3,17 +3,16 @@ from calc import calc
 
 class TestCalc(unittest.TestCase):
 
-def test_add_positive_integers(self):
+ def test_add_positive_integers(self):
     result = calc("+ 5 3")
     self.assertEqual(result, 8)
-    pass
 
-def test_divide_by_zero(self):
+ def test_divide_by_zero(self):
     with self.assertRaises(ZeroDivisionError):
         calc("/ 5 0")
 
 
-def test_modulo_negative_numbers(self):
+ def test_modulo_negative_numbers(self):
     result = calc("% -15 4")
     self.assertEqual(result, 1)
 if __name__ == "__main__":
