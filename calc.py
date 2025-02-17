ce09d91369502ed5638bc9087c92a40ae4d5a7ec
Robert Ngo <robertncl@gmail.com>
2025-02-17 14:18:34 +0800

    add comment

--- calc.py
+++ calc.py
@@ -1,17 +1,28 @@
-import operator                                 
- 
 def calc(to_solve):
+    """
+    Perform a mathematical operation on two numbers.
+
+    This function takes a string input representing a mathematical operation
+    and returns the result of that operation.
+
+    Parameters:
+    to_solve (str): A string in the format "operator num1 num2"
+                    where operator is one of +, -, *, /, **, or %,
+                    and num1 and num2 are integers.
+
+    Returns:
+    float: The result of the mathematical operation.
+
+    """
     operations = {'+': operator.add,            
                   '-': operator.sub,
                   '*': operator.mul,
                   '/': operator.truediv,        
                   '**': operator.pow,
                   '%': operator.mod}
- 
+
     op, first_s, second_s = to_solve.split()    
     first = int(first_s)
     second = int(second_s)
- 
-    return operations[op](first, second)        
- 
-print(calc('+ 2 3'))
+
+    return operations[op](first, second)
