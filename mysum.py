dd6b7aa2ffdae067632a0cb2635d4b516f65d52b
Robert Ngo <robertncl@gmail.com>
2025-06-27 16:15:49 +0800

    Add type annotations to mysum function and update return value for empty input

--- mysum.py
+++ mysum.py
@@ -1,11 +1,12 @@
-def mysum(*items):
-    if not items:          
-        return items
+def mysum(*items: int) -> int:
+    """Return the sum of all arguments."""
+    if not items:
+        return 0
     output = items[0]
     for item in items[1:]:
-        output += item     
+        output += item
     return output
- 
+
 print(mysum())
 print(mysum(10, 20, 30, 40))
 print(mysum('a', 'b', 'c', 'd'))
