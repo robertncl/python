cce68bf5f94fe7cb952db1913cacde9d1b1ac600
Robert Ngo <robertncl@gmail.com>
2025-06-27 16:16:02 +0800

    Add type annotations to Sentence class methods for improved clarity and maintainability

--- sentence_gen.py
+++ sentence_gen.py
@@ -1,19 +1,21 @@
 import re
 import reprlib
+from typing import Iterator
 
 RE_WORD = re.compile(r'\w+')
 
 
 class Sentence:
+    """A sequence of words extracted from a text, supporting iteration."""
 
-    def __init__(self, text):
+    def __init__(self, text: str) -> None:
         self.text = text
         self.words = RE_WORD.findall(text)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return 'Sentence(%s)' % reprlib.repr(self.text)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str]:
         for word in self.words:  
             yield word  
         return  
