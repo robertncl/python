5bac89c131064e75ccaf5dbf753fa9188c20c183
Robert Ngo <robertncl@gmail.com>
2025-06-27 16:16:12 +0800

    Add type annotations to the Sentence class constructor and methods for improved clarity and maintainability

--- sentence.py
+++ sentence.py
@@ -5,16 +5,17 @@ RE_WORD = re.compile(r'\w+')
 
 
 class Sentence:
+    """A sequence of words extracted from a text."""
 
-    def __init__(self, text):
+    def __init__(self, text: str) -> None:
         self.text = text
         self.words = RE_WORD.findall(text)  
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> str:
         return self.words[index]  
 
-    def __len__(self):  
+    def __len__(self) -> int:  
         return len(self.words)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return 'Sentence(%s)' % reprlib.repr(self.text)  
