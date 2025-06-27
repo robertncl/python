5e74967353914414c69a4d32ed5736e1b165c86b
Robert Ngo <robertncl@gmail.com>
2025-06-27 16:11:00 +0800

    Refactor type annotations in randompickload.py for improved clarity

--- randompickload.py
+++ randompickload.py
@@ -1,6 +1,6 @@
-from typing import Protocol, runtime_checkable, Any, Iterable
+from typing import Protocol, runtime_checkable
 from randompick import RandomPicker
 
-@runtime_checkable  
-class LoadableRandomPicker(RandomPicker, Protocol):  
-    def load(self, Iterable) -> None: ...  
+@runtime_checkable
+class LoadableRandomPicker(RandomPicker, Protocol):
+    def load(self, items) -> None: ... 
