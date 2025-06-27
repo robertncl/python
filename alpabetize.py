ef40eb5b35b95772f0935d32542c3272f88d6b5c
Robert Ngo <robertncl@gmail.com>
2025-06-27 16:20:23 +0800

    Add type annotations and docstrings to functions in alpabetize.py and flatten.py for improved clarity and maintainability

--- alpabetize.py
+++ alpabetize.py
@@ -1,4 +1,5 @@
 import operator
+from typing import List, Dict
 
 PEOPLE = [{'first': 'Reuven', 'last': 'Lerner',
            'email': 'reuven@lerner.co.il'},
@@ -8,7 +9,8 @@ PEOPLE = [{'first': 'Reuven', 'last': 'Lerner',
            'email': 'president@kremvax.ru'}
           ]
 
-def alphabetize_names(list_of_dicts):
+def alphabetize_names(list_of_dicts: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    """Return a list of dictionaries sorted by last, then first name."""
     return sorted(list_of_dicts,
         key=operator.itemgetter('last', 'first'))
 
--- flatten.py
+++ flatten.py
@@ -1,6 +1,9 @@
-def flatten(mylist):
+from typing import List, Any
+
+def flatten(mylist: List[List[Any]]) -> List[Any]:
+    """Flatten a list of lists into a single list."""
     return [one_element
-            for one_sublist in mylist           
-             for one_element in one_sublist]    
- 
-print(flatten([[1,2], [3,4]]))
+            for one_sublist in mylist
+            for one_element in one_sublist]
+
+print(flatten([[1, 2], [3, 4]]))
