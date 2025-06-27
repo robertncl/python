abae17ef0731be09bf241cea1c73855b02200693
Robert Ngo <robertncl@gmail.com>
2025-06-27 16:11:31 +0800

    update

--- simplify.py
+++ simplify.py
@@ -1,13 +1,12 @@
 import unicodedata
-import string
 
 
 def shave_marks(txt):
     """Remove all diacritic marks"""
-    norm_txt = unicodedata.normalize('NFD', txt)  
+    norm_txt = unicodedata.normalize('NFD', txt)
     shaved = ''.join(c for c in norm_txt
-                     if not unicodedata.combining(c))  
-    return unicodedata.normalize('NFC', shaved)  
+                     if not unicodedata.combining(c))
+    return unicodedata.normalize('NFC', shaved)
 
 
 Greek = 'Ζέφυρος, Zéfiro'
